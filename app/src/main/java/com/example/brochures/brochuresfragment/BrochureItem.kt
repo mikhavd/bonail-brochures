6cdbeda8cc5267604a78b15327b7924ef5474beb
MIkhail <avdeev.m92@gmail.com>
2022-04-23 19:15:57 +0300

    brochure item with image

--- app/src/main/java/com/example/brochures/brochuresfragment/BrochureItem.kt
+++ app/src/main/java/com/example/brochures/brochuresfragment/BrochureItem.kt
@@ -5,5 +5,6 @@ import android.graphics.drawable.Drawable
 data class BrochureItem(
     val name: String,
     val image: Drawable?, //todo RecyclerView with Image in Android Studio https://www.youtube.com/watch?v=x-2qtxmxYE8
-    val retailerName: String
+    val retailerName: String,
+    val imageURL: String
 )
--- app/src/main/java/com/example/brochures/brochuresfragment/BrochuresViewModel.kt
+++ app/src/main/java/com/example/brochures/brochuresfragment/BrochuresViewModel.kt
@@ -10,6 +10,8 @@ import androidx.lifecycle.ViewModel
  */
 class BrochuresViewModel : ViewModel() {
 
+    private val TEST_IMAGE_URL: String =
+        "https://content-media.bonial.biz/64aa6e5e-7a36-40a8-9e65-c90efe0df3f5/4f8f7ba7-72a5-47cb-82e7-62335db48c81.png"
     private val brochures: MutableLiveData<List<BrochureItem>> by lazy(::loadBrochures)
 
     fun getBrochures(): LiveData<List<BrochureItem>> {
@@ -19,7 +21,7 @@ class BrochuresViewModel : ViewModel() {
     private fun loadBrochures(): MutableLiveData<List<BrochureItem>> {
         //todo this is a stub
         return MutableLiveData<List<BrochureItem>>().apply {
-            value = (arrayListOf(BrochureItem("name", null, "retailer_name")))
+            value = (arrayListOf(BrochureItem("name", image = null, TEST_IMAGE_URL, "retailer_name")))
         }
     }
 }
